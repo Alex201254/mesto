bf8cc507dd2bdc904672533a2cf2ca951b60f82a
Alex201254 <santa201254@yandex.ru>
2021-10-01 09:00:30 +0300

    fixing 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,31 @@
 const modalWindow = document.querySelector('.popup');
 const modalWindowCloseBtn = modalWindow.querySelector('.popup__close');
 const editBtn = document.querySelector('.profile__edit-button');
-const saveBtn = modalWindow.querySelector('.form__submit-btn_action_save');
 const caption = document.querySelector('.profile__caption');
-const form = modalWindow.querySelector('.input');
-const user = modalWindow.querySelector('.input__text_type_name');
-const status = modalWindow.querySelector('.input__text_type_status');
+const form = modalWindow.querySelector('.form');
+const user = modalWindow.querySelector('.form__text_type_name');
+const status = modalWindow.querySelector('.form__text_type_status');
 const name = document.querySelector('.profile__name');
 const statuss = document.querySelector('.profile__status');
 
-function toggleModalWindow() {
-  modalWindow.classList.toggle('popup_is-opened');
+function openModalWindow() {
+  modalWindow.classList.add('popup_is-opened');
   user.value = name.textContent;
   status.value = statuss.textContent;
 }
 
-editBtn.addEventListener('click', toggleModalWindow);
-modalWindowCloseBtn.addEventListener('click', toggleModalWindow);
+function closeModalWindow() {
+  modalWindow.classList.remove('popup_is-opened');
+}
+
+editBtn.addEventListener('click', openModalWindow);
+modalWindowCloseBtn.addEventListener('click', closeModalWindow);
 
 function saveUser(evt) {
   evt.preventDefault();
   name.textContent = user.value;
   statuss.textContent = status.value;
-  toggleModalWindow();
+  closeModalWindow();
 }
 
 form.addEventListener('submit', saveUser);
