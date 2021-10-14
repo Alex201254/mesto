7da3179903813ef425ca92dccc8b59ad3c57bb49
Alex201254 <santa201254@yandex.ru>
2021-10-14 20:32:07 +0300

    fix: исправить html и js

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,44 @@
-const modalWindow = document.querySelector('.popup');
-const modalWindow1 = document.querySelector('.popup_profile');
-const modalWindow2 = document.querySelector('.popup_elements');
-const popupContent = document.querySelectorAll('.popup__content');
-const modalWindowCloseBtn = document.querySelectorAll('.popup__close');
+const profilePopup = document.querySelector('.popup_profile');
+const newCardPopup = document.querySelector('.popup_new-card');
+const imagePopup = document.querySelector('.popup_image');
+const profileCloseBtn = profilePopup.querySelector('.popup__close');
+const newCardCloseBtn = newCardPopup.querySelector('.popup__close');
+const imageCloseBtn = imagePopup.querySelector('.popup__close');
 const editBtn = document.querySelector('.profile__edit-button');
 const caption = document.querySelector('.profile__caption');
-const form1 = modalWindow.querySelector('.form');
-const form2 = modalWindow1.querySelector('.form');
-const user = form1.querySelector('.form__text_type_name');
-const status = form1.querySelector('.form__text_type_status');
-const place = form2.querySelector('.form__text_type_name');
-const link = form2.querySelector('.form__text_type_status');
+const profileForm = profilePopup.querySelector('.form');
+const newCardForm = newCardPopup.querySelector('.form');
+const user = profileForm.querySelector('.form__text_type_name');
+const status = profileForm.querySelector('.form__text_type_status');
+const place = newCardForm.querySelector('.form__text_type_name');
+const link = newCardForm.querySelector('.form__text_type_status');
 const name = document.querySelector('.profile__name');
 const statuss = document.querySelector('.profile__status');
 const addBtn = document.querySelector('.profile__add-button');
 const elementsCont = document.querySelector('.elements__container');
 const elementTemplate = document.querySelector('#card-template').content;
 const elementPhoto = elementTemplate.querySelector('.element__photo');
-const popupImg = modalWindow2.querySelector('.popup__image');
-const popupPlace = modalWindow2.querySelector('.popup__place');
+const popupImg = imagePopup.querySelector('.popup__image');
+const popupPlace = imagePopup.querySelector('.popup__place');
 
 
 
 function openModalWindow(popup) {
   popup.classList.add('popup_is-opened');
-  if(popup === modalWindow) {
-    user.value = name.textContent;
-    status.value = statuss.textContent;
-  }
 }
 
 function closeModalWindow(popup) {
   popup.classList.remove('popup_is-opened');
 }
 
-editBtn.addEventListener('click',()=> openModalWindow(modalWindow));
-addBtn.addEventListener('click', ()=> openModalWindow(modalWindow1));
-modalWindowCloseBtn[0].addEventListener('click',()=> closeModalWindow(modalWindow));
-modalWindowCloseBtn[1].addEventListener('click',()=> closeModalWindow(modalWindow1));
-modalWindowCloseBtn[2].addEventListener('click',()=> closeModalWindow(modalWindow2));
+editBtn.addEventListener('click',()=> {
+  user.value = name.textContent;
+  status.value = statuss.textContent;
+  openModalWindow(profilePopup)});
+addBtn.addEventListener('click', ()=> openModalWindow(newCardPopup));
+profileCloseBtn.addEventListener('click',()=> closeModalWindow(profilePopup));
+newCardCloseBtn.addEventListener('click',()=> closeModalWindow(newCardPopup));
+imageCloseBtn.addEventListener('click',()=> closeModalWindow(imagePopup));
 
 
 
@@ -46,36 +46,41 @@ function saveUser(evt) {
   evt.preventDefault();
   name.textContent = user.value;
   statuss.textContent = status.value;
-  closeModalWindow(modalWindow);
+  closeModalWindow(profilePopup);
 }
 
-function renderElement(placeValue, linkValue) {
-  const el = elementTemplate.querySelector('.element').cloneNode(true);
-  el.querySelector('.element__photo').src = linkValue;
-  el.querySelector('.element__place').textContent = placeValue;
-  const elementLike = el.querySelector('.element__like');
+function createCard(placeValue, linkValue) {
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  const cardImage = element.querySelector('.element__photo');
+  cardImage.src = linkValue;
+  cardImage.alt = placeValue;
+  element.querySelector('.element__place').textContent = placeValue;
+  const elementLike = element.querySelector('.element__like');
   elementLike.addEventListener('click', function(evt) {
     const eventTarget = evt.target;
     eventTarget.classList.toggle('element__like_active');
   })
-  const elementDel = el.querySelector('.element__delete');
+  const elementDel = element.querySelector('.element__delete');
   elementDel.addEventListener('click', function() {
     const listItem = elementDel.closest('.element');
     listItem.remove();
   })
-  const elementPhoto = el.querySelector('.element__photo');
-  elementPhoto.addEventListener('click', ()=> {
+  cardImage.addEventListener('click', ()=> {
+    cardImage.alt = placeValue;
     popupImg.src = linkValue;
     popupPlace.textContent = placeValue;
-    openModalWindow(modalWindow2);
-  });
-  if(placeValue === place.value){
+    openModalWindow(imagePopup);
+  })
+  return element;
+}
+
+function renderElement(placeValue, linkValue, prepend=true) {
+  const el = createCard(placeValue, linkValue);
+  if(prepend){
     elementsCont.prepend(el);
   }else{
     elementsCont.append(el);
   }
-
-
 }
 
 function saveImg(evt) {
@@ -83,11 +88,11 @@ function saveImg(evt) {
   renderElement(place.value, link.value);
   place.value = '';
   link.value = '';
-  closeModalWindow(modalWindow1);
+  closeModalWindow(newCardPopup);
 }
 
-form1.addEventListener('submit', saveUser);
-form2.addEventListener('submit', saveImg);
+profileForm.addEventListener('submit', saveUser);
+newCardForm.addEventListener('submit', saveImg);
 
 const initialCards = [
   {
@@ -116,8 +121,6 @@ const initialCards = [
   }
 ];
 
-for (let i = 0; i < initialCards.length; i++) {
-  renderElement(initialCards[i].name, initialCards[i].link);
-}
+initialCards.forEach(item => renderElement(item.name, item.link));
 
 
